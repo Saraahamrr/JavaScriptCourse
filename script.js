f5edda8dec0dbf15ae4c18d97ccf4ca601454fe6
sarah <saraahamrr98@gmail.com>
2024-12-21 23:46:10 +0200

    Lab1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// console.log("Hello Eng/menna this is Sarah Amr's Lab1");
+console.log("Hello Eng/menna this is Sarah Amr's Lab1");
 
 
 
@@ -11,48 +11,48 @@
 // // // Output: 3, 2, -7
 
 
-// function sort (num1 , num2 , num3) {
+function sort (num1 , num2 , num3) {
 
-//     if (num1 > num2 && num1 > num3){
-//         max = num1
-//         if (num2 > num3 ){
-//             mid = num2;
-//             min = num3;
-//         }
-//         else {
-//             mid = num3
-//             min = num2
-//         }
-//     }
-//     else if (num2 > num1 && num2 > num3 ){
-//             max = num2
-//             if (num1 > num3){
-//                 mid = num1
-//                 min= num3
-//             }
-//             else{
-//                 mid = num3
-//                 min = num1
-//             }
-//     }
-//     else{
-//         max = num3
-//         if(num2 > num1){
-//             mid = num2
-//             min = num1
-//         }
-//         else{
-//             mid = num1
-//             min = num2
-//         }
-//     }
+    if (num1 > num2 && num1 > num3){
+        max = num1
+        if (num2 > num3 ){
+            mid = num2;
+            min = num3;
+        }
+        else {
+            mid = num3
+            min = num2
+        }
+    }
+    else if (num2 > num1 && num2 > num3 ){
+            max = num2
+            if (num1 > num3){
+                mid = num1
+                min= num3
+            }
+            else{
+                mid = num3
+                min = num1
+            }
+    }
+    else{
+        max = num3
+        if(num2 > num1){
+            mid = num2
+            min = num1
+        }
+        else{
+            mid = num1
+            min = num2
+        }
+    }
 
-//     console.log("Here are the three numbers sorted in console");
-//     console.log(max,mid,min);
-//     return `${max}, ${mid}, ${min}`;
-// }
+    console.log("Here are the three numbers sorted in console");
+    console.log(max,mid,min);
+    return `${max}, ${mid}, ${min}`;
+}
 
-// alert ("The sorted Numbers are : " + sort(3,-7,2))
+alert ("The sorted Numbers are : " + sort(3,-7,2))
 
 
 
@@ -61,33 +61,33 @@
 // //      and for the multiples of five print "Buzz".
 // //     For numbers which are multiples of both three and five print "FizzBuzz".
 
-// for (i=1 ; i <= 100 ; i++) {
-//     if (i % 3 == 0 && i % 5 == 0){
-//         console.log("FizzBuzz");
-//     }
-//     else if ( i % 3 == 0){
-//         console.log("Fizz");
-//     }
-//     else if ( i % 5 == 0){
-//         console.log("Buzz");
-//     }
-//     else{
-//         console.log(i);
-//     }
+for (i=1 ; i <= 100 ; i++) {
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz");
+    }
+    else if ( i % 3 == 0){
+        console.log("Fizz");
+    }
+    else if ( i % 5 == 0){
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+    }
 
-// }
+}
 
 
 // // // 3)  Write a JavaScript program to sum the multiples of 3 and 5 under 1000.
 
-// var sum = 0
-// for (i=1 ; i < 1000; i++) {
-//     if (i % 3 == 0 && i % 5 == 0){
-//         sum += i;
-//     }
-// }
+var sum = 0
+for (i=1 ; i < 1000; i++) {
+    if (i % 3 == 0 && i % 5 == 0){
+        sum += i;
+    }
+}
 
-// console.log("the sum is :" , sum)
+console.log("the sum is :" , sum)
 
 
 // // // 4)  Write a JavaScript program where the program takes:
@@ -137,11 +137,4 @@ else{
         alert("Game over")
         }
 }
-
-
-
-
-
-// for(i = 1 ; i <=3 ; i++){
-// }
 
